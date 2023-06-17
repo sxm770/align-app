5cc1f6be2d418cc81d8e0fe3126c9d3f28836ba6
Sammie <110550743+sxm770@users.noreply.github.com>
2023-06-17 16:01:49 -0500

    cleaning code

--- app/src/main/java/com/example/alignhoroscope/MainActivity.kt
+++ app/src/main/java/com/example/alignhoroscope/MainActivity.kt
@@ -5,11 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,10 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign.Companion.Center
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -113,14 +109,14 @@ fun HoroscopeScreen(
     zodiacSign: String,
 ) {
 //     Get the ViewModel
-//    val viewModel: HoroscopeViewModel = hiltViewModel()
+    val viewModel: HoroscopeViewModel = hiltViewModel()
 
 //     Observe the horoscope state
-//    val horoscope by viewModel.horoscope.observeAsState("")
+    val horoscope by viewModel.horoscope.observeAsState("")
 
 //     Fetch the horoscope when the screen is displayed
     LaunchedEffect(zodiacSign) {
-//        viewModel.fetchHoroscope(zodiacSign)
+        viewModel.fetchHoroscope(zodiacSign)
     }
 
 //    Text(text = "Horoscope for $zodiacSign: $horoscope")
