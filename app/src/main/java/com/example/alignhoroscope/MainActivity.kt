f3d8f5fbc15e1bdc790977ca68f461bb24a6a32c
Sammie <110550743+sxm770@users.noreply.github.com>
2023-06-17 14:00:41 -0500

    cleaned up code

--- app/src/main/java/com/example/alignhoroscope/MainActivity.kt
+++ app/src/main/java/com/example/alignhoroscope/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.alignhoroscope
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -19,19 +18,17 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign.Companion.Center
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.alignhoroscope.ui.theme.AlignhoroscopeTheme
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import java.util.*
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,36 +36,48 @@ class MainActivity : ComponentActivity() {
             NavHost(navController = navController, startDestination = "main") {
                 composable("main") { MainPage { zodiacSign -> navController.navigate("horoscope/$zodiacSign") } }
                 composable(
-                    route ="horoscope/{zodiacSign}",
-                    arguments = listOf(navArgument("zodiacSign") { }),
-                    content = {
-                        Text(text = "${it.arguments?.getString("zodiacSign")}")
-                    }
-                )
-            }
-        }
-    }
-
-    @Composable
-    private fun MainPage( onHoroscopeClicked : (String) -> Unit ) {
-        AlignhoroscopeTheme {
-            Surface(
-                modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.verticalScroll(rememberScrollState())
-                ) {
-                    HomePageHeader()
-                    ZodiacList(onHoroscopeClicked)
+                    route = "horoscope/{zodiacSign}",
+                    arguments = listOf(navArgument("zodiacSign") { })
+                ) { backStackEntry ->
+                    val zodiacSign = backStackEntry.arguments?.getString("zodiacSign") ?: ""
+                    HoroscopeScreen(zodiacSign)
                 }
             }
         }
     }
 }
 
-val zodiacSigns = listOf("aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio","sagittarius", "capricorn", "aquarius", "pisces")
-val days = listOf("today", "yesterday", "tomorrow")
+@Composable
+private fun MainPage(onHoroscopeClicked: (String) -> Unit) {
+    AlignhoroscopeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.verticalScroll(rememberScrollState())
+            ) {
+                HomePageHeader()
+                ZodiacList(onHoroscopeClicked)
+            }
+        }
+    }
+}
+
+val zodiacSigns = listOf(
+    "aries",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "libra",
+    "scorpio",
+    "sagittarius",
+    "capricorn",
+    "aquarius",
+    "pisces"
+)
 
 @Composable
 fun HomePageHeader() {
@@ -93,9 +102,25 @@ fun ZodiacList(onHoroscopeClicked: (String) -> Unit) {
 }
 
 @Composable
-fun ZodiacSVG(zodiacSign : String) {
-    val image: Painter = painterResource(id = R.drawable.aquarius)
-    Image(painter = image, contentDescription = "$zodiacSign", modifier = Modifier.size(100.dp))
+fun ZodiacSVG(zodiacSign: String) {
+    val imageResId = when (zodiacSign.lowercase(Locale.ROOT)) {
+        "aries" -> R.drawable.aries
+        "taurus" -> R.drawable.taurus
+        "gemini" -> R.drawable.gemini
+        "cancer" -> R.drawable.cancer
+        "leo" -> R.drawable.leo
+        "virgo" -> R.drawable.virgo
+        "libra" -> R.drawable.libra
+        "scorpio" -> R.drawable.scorpio
+        "sagittarius" -> R.drawable.sagittarius
+        "capricorn" -> R.drawable.capricorn
+        "aquarius" -> R.drawable.aquarius
+        "pisces" -> R.drawable.pisces
+        else -> R.drawable.gemini // fallback image
+    }
+
+    val image: Painter = painterResource(id = imageResId)
+    Image(painter = image, contentDescription = zodiacSign, modifier = Modifier.size(100.dp))
 }
 
 @Composable
@@ -103,19 +128,24 @@ fun ZodiacCard(
     zodiacSign: String,
     onHoroscopeClicked: (String) -> Unit,
 ) {
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.clickable {onHoroscopeClicked(zodiacSign)}
-            ) {
-            Text(text = zodiacSign.capitalize())
-            ZodiacSVG(zodiacSign)
-        }
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable { onHoroscopeClicked(zodiacSign) }
+    ) {
+        Text(
+            text = zodiacSign.replaceFirstChar {
+                if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString()
+            },
+            modifier = Modifier.padding(end = 8.dp)
+        )
+        ZodiacSVG(zodiacSign)
     }
+}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    AlignhoroscopeTheme {
-        HomePageHeader()
-    }
+fun HoroscopeScreen(
+    zodiacSign: String,
+) {
+    Text(text = "what up $zodiacSign")
 }
--- app/src/main/java/com/example/alignhoroscope/util/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.alignhoroscope.util
-
-sealed class Resource<T> {
-    data class Success<T>(val data: T) : Resource<T>()
-    data class Error<T>(val message: String) : Resource<T>()
-    class Loading<T> : Resource<T>()
-}
